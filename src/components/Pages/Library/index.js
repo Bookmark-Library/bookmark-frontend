44dd760caeb6525fc2c96557e18061677c4f406d
fanie-evrard <evrard.fanie@gmail.com>
2023-04-21 00:26:14 +0200

    css library - bookmark - creation component menuleft for the library

--- src/components/Pages/Library/index.js
+++ src/components/Pages/Library/index.js
@@ -1,7 +1,8 @@
 import { useSelector } from 'react-redux';
-import { Link, Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 import All from './All';
+import MenuLeft from './MenuLeft';
 
 import './styles.scss';
 
@@ -15,28 +16,7 @@ function Library() {
   return (
     <div className="container biblio mt-5">
       <div className="row">
-        <div className="col-12 col-md-4 searchBiblio">
-          <Link
-            className="btn btn-warning btn-md"
-            to="/ajout-livre"
-            role="button"
-          >
-            ajouter un livre
-          </Link>
-          <ul className="navLibrary mt-3">
-            <li><Link to="/bibliotheque">Tous mes livres</Link></li>
-            <li><Link to="/bibliotheque/lus">Livres lus</Link></li>
-            <li><Link to="/bibliotheque/a-lire">Pile à lire</Link></li>
-            <li><Link to="/bibliotheque/envies">Mes envies</Link></li>
-            <li><Link to="/bibliotheque/favoris">Coups de coeurs</Link></li>
-          </ul>
-          <select className="form-select form-select-sm" aria-label=".form-select-sm example">
-            <option defaultValue>Recherche par...</option>
-            <option value="1">Titre</option>
-            <option value="2">Auteur</option>
-            <option value="3">Genre</option>
-          </select>
-        </div>
+        <MenuLeft />
         <All />
       </div>
     </div>
