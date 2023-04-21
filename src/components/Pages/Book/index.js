3c0616732e99124a96710188ddf49a8f32c86dfb
fanie-evrard <evrard.fanie@gmail.com>
2023-04-21 21:08:20 +0200

    css for book page and bookmark

--- src/components/Pages/Book/index.js
+++ src/components/Pages/Book/index.js
@@ -1,5 +1,5 @@
 import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import BookTab from './BookTab';
 
 import './styles.scss';
@@ -11,29 +11,35 @@ function Book() {
   const bookToDisplay = libraries.find((book) => book.book.id == id);
   // console.log(bookToDisplay);
   return (
-    <main className="container">
+    <div className="container">
       <div className="book-content container mt-5">
+        <Link
+          to="/bibliotheque"
+          className="linkRetour"
+        >
+          Retour à la bibliothèque
+        </Link>
         <div className="row">
-          <div className="book-image col-md-3">
-            <h2 className="book-title">{bookToDisplay.book.title}</h2>
-            <img className="book-image" src={bookToDisplay.book.image} alt="" />
+          <div className="col-md-3">
+            <img className="book-image img-fluid" src={bookToDisplay.book.image} alt="" />
           </div>
           <div className="book-synopsy d-flex flex-column col-md-9">
+            <h3 className="book-title">{bookToDisplay.book.title}</h3>
+            <p className="book-description">{bookToDisplay.book.summary}
+            </p>
             <div className="book-author">
               {bookToDisplay.book.authors.map((author) => (
-                <p key={author.lastname} className="book-author">{author.lastname} {author.firstname}</p>
+                <p key={author.lastname} className="book-author"><strong>Auteur(s) :</strong> {author.lastname} {author.firstname}</p>
               ))}
 
             </div>
 
-            <p className="book-editor">{bookToDisplay.book.editor}</p>
-            <p className="book-description">{bookToDisplay.book.summary}
-            </p>
+            <p className="book-editor"><strong>Editeur :</strong> {bookToDisplay.book.editor}</p>
           </div>
           <BookTab {...bookToDisplay} />
         </div>
       </div>
-    </main>
+    </div>
 
   );
 }
--- src/components/Pages/Library/MenuLeft/index.js
+++ src/components/Pages/Library/MenuLeft/index.js
@@ -1,4 +1,9 @@
 import { Link } from 'react-router-dom';
+import bookHeart from '../../../../assets/images/favoris-01.svg';
+import bookWish from '../../../../assets/images/wishlist.svg';
+import bookLu from '../../../../assets/images/lu.svg';
+import bookLire from '../../../../assets/images/lire.svg';
+
 import './styles.scss';
 
 function MenuLeft() {
@@ -9,14 +14,15 @@ function MenuLeft() {
         to="/ajout-livre"
         role="button"
       >
-        ajouter un livre
+        Ajouter un livre
       </Link>
       <ul className="navLibrary mt-3">
         <li className="allBookLink"><Link to="/bibliotheque">Tous mes livres</Link></li>
-        <li className="bookFinished"><Link to="/bibliotheque/lus">Livres lus</Link></li>
-        <li className="bookPurchased"><Link to="/bibliotheque/a-lire">Pile à lire</Link></li>
-        <li className="bookWish"><Link to="/bibliotheque/envies">Mes envies </Link></li>
-        <li className="bookFavorite"><Link to="/bibliotheque/favoris">Coups de coeurs</Link></li>
+        <li className="bookFinished"><img src={bookLu} className="img-fluid" alt="..." /><Link to="/bibliotheque/lus">Livres lus</Link></li>
+        <li className="bookPurchased"><img src={bookLire} className="img-fluid" alt="..." /><Link to="/bibliotheque/a-lire">Pile à lire</Link></li>
+        <li className="bookPurchased"><Link to="/bibliotheque/livres-achetes">Achetés</Link></li>
+        <li className="bookWish"><img src={bookWish} className="img-fluid" alt="..." /><Link to="/bibliotheque/envies">Mes envies </Link></li>
+        <li className="bookFavorite"><img src={bookHeart} className="img-fluid" alt="..." /><Link to="/bibliotheque/favoris">Coups de coeur</Link></li>
       </ul>
       <select className="form-select form-select-sm" aria-label=".form-select-sm example">
         <option defaultValue>Recherche par...</option>
