7169417e15562e744c1c015cb71bf01eb4160226
fanie-evrard <evrard.fanie@gmail.com>
2023-04-28 14:35:25 +0200

    css user profil

--- src/components/Pages/User/index.js
+++ src/components/Pages/User/index.js
@@ -2,7 +2,7 @@
 import './styles.scss';
 // import avatar from '../../../assets/images/user-128.png';
 import { useDispatch, useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
+import { Navigate, Link } from 'react-router-dom';
 import Field from '../../Field';
 import { changeInput } from '../../../actions/book';
 import {
@@ -37,22 +37,22 @@ const User = () => {
           <h2>Informations personnelles</h2>
           <div className="row">
             <img src={`${url}${image}/${avatar}`} className="img-thumbnail img-fluid mx-auto d-block" alt="..." />
-            <button
-              type="button"
-              className="btn btn-link"
+            <Link
+              className="linkPassword"
               onClick={() => {
                 dispatch(openModal());
               }}
             >
               Modifier votre mot de passe
-            </button>
+            </Link>
           </div>
         </div>
 
         <form className="row g-3">
           <div className="col-md-12">
             <div className="col-md-12">
-              <input type="file" name="avatar" onChange={(e) => handleChange(e)} />
+              <input className="form-control form-control-sm" name="avatar" id="name" type="file" onChange={(e) => handleChange(e)} />
+              {/* <input type="file" name="avatar" onChange={(e) => handleChange(e)} /> */}
             </div>
             <div className="row">
               <div className="col-md-6">
@@ -78,25 +78,23 @@ const User = () => {
                 />
               </div>
               <div className="col-12">
-                <button type="button" className="btn btn-warning dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Enregistrer</button>
-                <div className="dropdown-menu mr-2">
-                  <button
-                    type="submit"
-                    className="dropdown-item"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      dispatch(updateUserInApi());
-                      dispatch(updateUserAvatardInApi());
-                      dispatch(updateUser());
-                      // dispatch(submitLogout());
 
-                      localStorage.setItem('pseudo', alias);
-                      localStorage.setItem('email', email);
-                    }}
-                  >
-                    Oui, je veux enregistrer les modifications
-                  </button>
-                </div>
+                <button
+                  type="submit"
+                  className="btn btn-warning"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    dispatch(updateUserInApi());
+                    dispatch(updateUserAvatardInApi());
+                    dispatch(updateUser());
+                    // dispatch(submitLogout());
+
+                    localStorage.setItem('pseudo', alias);
+                    localStorage.setItem('email', email);
+                  }}
+                >
+                  Enregistrer
+                </button>
                 <button type="button" className="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Supprimer</button>
                 <div className="dropdown-menu">
                   <button
